afc2144ef23e00e1238f2fd9d9d3f7f44b939144
Eldest Pasirula <aknutman@gmail.com>
2022-01-24 21:31:50 +0700

    use docker based hostname

--- simbaja/simple-pro-strapi-02/config/database.js
+++ simbaja/simple-pro-strapi-02/config/database.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
     connection: {
         client: 'postgres',
         connection: {
-            host: env('DATABASE_HOST', '127.0.0.1'),
+            host: env('DATABASE_HOST', 'simbaja_postgres_1'),
             port: env.int('DATABASE_PORT', 5432),
             database: env('DATABASE_NAME', 'simple-pro-strapi-02'),
             user: env('DATABASE_USERNAME', 'postgres'),
