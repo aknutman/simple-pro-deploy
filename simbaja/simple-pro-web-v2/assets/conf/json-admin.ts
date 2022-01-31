98983adfd80ca9dfe22415020b7d9e748d0b1f8a
Eldest Pasirula <aknutman@gmail.com>
2022-01-31 23:43:07 +0700

    external admin conf

---
+++ simbaja/simple-pro-web-v2/assets/conf/json-admin.ts
@@ -0,0 +1,10 @@
+export const ADMIN_CONF = Object({
+    "strapi_url": "http://localhost:1337",
+    "ppSuggestionWeight": {
+        "functionalWeight": 0.35,
+        "outstandingWeight": 0.25,
+        "ratingWeight": 0.1,
+        "regionWeight": 0.3
+    },
+    "csvDelimiter": "|"
+});
